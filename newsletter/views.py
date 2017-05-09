6f349ddef1d27c735afff869b6f089601369914f
youhanlee <youhanlee@kaist.ac.kr>
2017-05-09 09:55:02 -0700

    good

--- newsletter/views.py
+++ newsletter/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render
 
 from .forms import SignUpForm, ContactForm
+from .models import SignUp
 
 
 # Create your views here.
@@ -28,6 +29,16 @@ def home(request):
         print(instance.timestamp)
         context = {
             "title": "Thank you"
+        }
+
+    if request.user.is_authenticated() and request.user.is_staff:
+        # print(SignUp.objects.all())
+        for instance in SignUp.objects.all():
+            print(instance)
+        # queryset = SignUp.objects.all().order_by('-timestamp').filter(full_name__icontains="youhan")
+        queryset = SignUp.objects.all().order_by('-timestamp')
+        context = {
+            "queryset": queryset
         }
 
     return render(request, "home.html", context)
